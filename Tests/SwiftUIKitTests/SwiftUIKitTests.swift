5216488449aeeeb1124d8de63777099e62373529
Cheng Zhang <showt2me@gmail.com>
2020-04-13 22:11:31 +0900

    [Project] Mark lib as .dynamic

--- Tests/SwiftUIKitTests/SwiftUIKitTests.swift
+++ Tests/SwiftUIKitTests/SwiftUIKitTests.swift
@@ -6,7 +6,7 @@ final class SwiftUIKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-    }
+     }
 
     static var allTests = [
         ("testExample", testExample),
