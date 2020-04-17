a4e9653503ea146515d786a542a6c266dccfdd9a
Cheng Zhang <showt2me@gmail.com>
2020-04-17 22:10:22 +0900

    Update ListDiffable for Codable.

--- Sources/SwiftUIKit/ListDiffable.swift
+++ Sources/SwiftUIKit/ListDiffable.swift
@@ -1,28 +1,39 @@
-import SwiftUI
+import CZUtils
 
 /**
  Alternative protocol of List `Identifierable`, which doesn't require `id` prop that possiblity conflicts with model prop.
  
- ### Example
- public struct Feed: ListDiffable {
-   public var diffId = UUID()
-   public var isLiked: Bool { didSet { updateDiffId() } }
- }
+ - Note:
+ For instances which conform to `Codable`, `diffId` will automatically be implemented with the string retrived from
+ all member variables of `self`, which is efficient for diffing.
  
  In List, `ForEach(feeds, id: \.diffId)`.
  */
 public protocol ListDiffable {
   /// Diffable id be used for List identifierable.
-  var diffId: UUID { get set }
-  
-  /// Updates `diffId` to trigger list cell reload.
-  mutating func updateDiffId()
+  var diffId: String { get }
 }
 
-public extension ListDiffable {
-  /// Updates `diffId` to trigger list reload.
-  mutating func updateDiffId() {
-    diffId = UUID()
+public extension ListDiffable where Self: Codable {
+  /// In Codable extension, `description` returns `self` dictionary version corresponding to
+  /// all member variables of `self`, which is efficient for diffing.
+  var diffId: String {
+    return description
   }
 }
+
+//public protocol ListDiffable {
+//  /// Diffable id be used for List identifierable.
+//  var diffId: UUID { get set }
+//
+//  /// Updates `diffId` to trigger list cell reload.
+//  mutating func updateDiffId()
+//}
+//
+//public extension ListDiffable {
+//  /// Updates `diffId` to trigger list reload.
+//  mutating func updateDiffId() {
+//    diffId = UUID()
+//  }
+//}
 
