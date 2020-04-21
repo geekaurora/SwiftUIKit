c6b168ab1a9fa08492cf1dd4f16567ac43341453
Cheng Zhang <showt2me@gmail.com>
2020-04-22 01:46:01 +0900

    Update SearchBar to handle searchBarTextDidBeginEditing().

--- Sources/SwiftUIKit/Views/SearchBar.swift
+++ Sources/SwiftUIKit/Views/SearchBar.swift
@@ -13,19 +13,23 @@ import UIKit
  */
 public struct SearchBar: UIViewRepresentable {
   public typealias OnSearch = (String?) -> Void
-  
+  public typealias OnTextDidBeginEditing = () -> Void
+
   //@Binding var text: String
   private let onSearch: OnSearch
+  private let onTextDidBeginEditing: OnTextDidBeginEditing?
   private let icon: UIImage?
   
   public init(icon: UIImage? = nil,
-              onSearch: @escaping OnSearch) {
+              onSearch: @escaping OnSearch,
+              onTextDidBeginEditing: OnTextDidBeginEditing? = nil) {
     self.icon = icon
     self.onSearch = onSearch
+    self.onTextDidBeginEditing = onTextDidBeginEditing
   }
   
   public func makeCoordinator() -> Coordinator {
-    return Coordinator(onSearch: onSearch)
+    return Coordinator(onSearch: onSearch, onTextDidBeginEditing: onTextDidBeginEditing)
   }
   
   public func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
@@ -43,20 +47,26 @@ public struct SearchBar: UIViewRepresentable {
   
   public class Coordinator: NSObject, UISearchBarDelegate {
     let onSearch: OnSearch
+    let onTextDidBeginEditing: OnTextDidBeginEditing?
     
     //        init(text: Binding<String>) {
     //            $text = text
     //        }
     
-    init(onSearch: @escaping OnSearch) {
+    public init(onSearch: @escaping OnSearch,
+                onTextDidBeginEditing: OnTextDidBeginEditing? = nil) {
       self.onSearch = onSearch
-      super.init()
+      self.onTextDidBeginEditing = onTextDidBeginEditing
     }
     
     public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
       //text = searchText
     }
-    
+        
+    public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+      onTextDidBeginEditing?()
+    }
+
     public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
       searchBar.resignFirstResponder()
       onSearch(searchBar.text)
