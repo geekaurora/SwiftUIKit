366defac528cac8dd9dbcfe1fffb2647977af545
Cheng Zhang <showt2me@gmail.com>
2021-04-17 22:15:59 +0900

    minor

--- Sources/SwiftUIKit/Views/FPSLabel.swift
+++ Sources/SwiftUIKit/Views/FPSLabel.swift
@@ -6,13 +6,10 @@ import CZUtils
  Wrap UIKit CZFPSLabel into SwiftUI.
  
  ### Usage
- 
  ```
  ```
  */
 public struct FPSLabel: UIViewRepresentable {
-  public typealias UIViewType = CZFPSLabel
-  
   public init() {}
   
   public func makeUIView(context: UIViewRepresentableContext<FPSLabel>) -> CZFPSLabel {
