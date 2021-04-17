95428352954fa1994bb31e2b8e836570bacef374
Cheng Zhang <showt2me@gmail.com>
2021-04-17 22:50:15 +0900

    [Major] works

--- Sources/SwiftUIKit/Views/FPSLabel.swift
+++ Sources/SwiftUIKit/Views/FPSLabel.swift
@@ -7,9 +7,17 @@ import CZUtils
  
  ### Usage
  ```
+ ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+   OtherView()
+         
+   FPSLabel()
+     .frame(minWidth: 0, maxWidth: FPSLabel.size.width, minHeight: 0, maxHeight: FPSLabel.size.height)
+ }
  ```
  */
 public struct FPSLabel: UIViewRepresentable {
+  public static let size = CZFPSLabel.Constant.size
+  
   public init() {}
   
   public func makeUIView(context: UIViewRepresentableContext<FPSLabel>) -> CZFPSLabel {
