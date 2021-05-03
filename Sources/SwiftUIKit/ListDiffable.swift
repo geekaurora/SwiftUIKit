39f36f97cf47c12918c8749fa21da1684ef33942
Cheng Zhang <showt2me@gmail.com>
2021-05-03 10:15:47 +0900

    Update ListDiffable to improve performance

--- Sources/SwiftUIKit/ListDiffable.swift
+++ Sources/SwiftUIKit/ListDiffable.swift
@@ -2,11 +2,11 @@ import Foundation
 import CZUtils
 
 /**
- Alternative protocol of List `Identifierable`, it requires `diffId` instead of `id` which possiblity conflicts with model prop.
+ Alternative protocol of List `Identifiable`, it requires `diffId` instead of `id` which possiblity conflicts with model prop.
  
- -  Note:
- For instances which conform to `Codable`, `diffId` will be automatically implemented with the string retrived from
- all member variables of `self`, which is efficient for diffing with respect to `self` property changes.
+ ### Note
+ - For instances which conforms to Identifiable protocol, `diffId` is implemented with `self.id`.
+ - If the instance properties are changable, you should override `diffId` by combining `id` with the changable properties.
  
  ### Usage
  
@@ -18,14 +18,32 @@ public protocol ListDiffable {
   var diffId: String { get }
 }
 
+/**
+ Convenient implementation of ListDiffable if Self conforms Identifiable protocol.
+ */
+public extension ListDiffable where Self: Identifiable {
+  var diffId: String {
+    assert(self.id is CustomStringConvertible, "self.id should conform CustomStringConvertible protocol.")
+    return String(describing:self.id)
+  }
+}
+
+/**
+ - Deprecated: description calculation of Codable significantly impacts performance.
+ 
+/// For instances which conform to `Codable`, `diffId` will be automatically implemented with the string retrived from
+/// all member variables of `self`, which is efficient for diffing with respect to `self` property changes.
+ 
 public extension ListDiffable where Self: Codable {
   /// In Codable extension, `description` returns `self` dictionary version corresponding to
   /// all member variables of `self`, which is efficient for diffing with respect to `self` property changes.
   var diffId: String {
-    return descriptionSortedByKey.MD5
-//    return description.MD5
+    // return descriptionSortedByKey.MD5
+    // return description.MD5
+    return UUID().uuidString
   }
 }
+ */
 
 /// Protocol composed of `Codable` and `ListDiffable`.
 ///
