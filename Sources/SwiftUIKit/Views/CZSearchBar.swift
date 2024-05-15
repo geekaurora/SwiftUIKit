23615dbbf99f4a34a6a898c730a791ba3899f578
cheng zhang <showt2me@gmail.com>
2024-05-14 18:22:02 -0700

    Minor update.

--- Sources/SwiftUIKit/Views/CZSearchBar.swift
+++ Sources/SwiftUIKit/Views/CZSearchBar.swift
@@ -1,31 +1,50 @@
 import SwiftUI
+import CZUtils
 
 public struct CZSearchBar: View {
-  public typealias OnSubmit = () -> Void
+  public typealias OnSubmit = (String) -> Void
 
-  private var text: Binding<String>
+  @State private var text = ""
+  @FocusState var isFocused: Bool
+  private var isFocusedFromInitializer = false
   private let placeholder: String
   private let onSubmit: OnSubmit
   private let disableAutocorrection: Bool
 
-  public  init(placeholder: String = "",
-               text: Binding<String>,
+  public init(placeholder: String = "",
+               text: String = "",
+               isFocused: Bool = false,
                onSubmit: @escaping OnSubmit,
                disableAutocorrection: Bool = true) {
-    self.text = text
     self.placeholder = placeholder
+    self.text = text
     self.onSubmit = onSubmit
     self.disableAutocorrection = disableAutocorrection
+    self.isFocusedFromInitializer = isFocused
   }
 
   public var body: some View {
     return HStack(alignment: .center, spacing: 10) {
       Image(systemName: "magnifyingglass")
-      TextField(self.placeholder, text: text)
+      TextField(self.placeholder, text: $text)
+        .focused($isFocused)
         .onSubmit {
-          onSubmit()
+          onSubmit(self.text)
         }
         .disableAutocorrection(self.disableAutocorrection)
+
+      if !self.text.isEmpty {
+        Image(systemName: "x.circle.fill")
+          .opacity(0.8)
+          .onTapGesture {
+            self.text = ""
+          }
+      }
+    }
+    .onAppear {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+        self.isFocused = isFocusedFromInitializer
+      }
     }
     .padding(.init(top: 17, leading: 20, bottom: 17, trailing: 20))
     .background(Color(red: 241.0/255.0, green: 243.0/255.0, blue: 244.0/255.0))
